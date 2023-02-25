86b6107b3b884fb575ae18c67a5faf9b89a6adab
vepardoa <violetaepardo@gmail.com>
2023-02-25 17:18:50 -0600

    Delete old support file

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+let playerCounter =0, computerCounter = 0;
+
 function getComputerChoice() {
     let choice = '';
     let choiceNum = Math.floor(Math.random() * 3) + 1;
@@ -12,34 +14,38 @@ function getComputerChoice() {
     }
     return choice;
 }
-
+// const greeting = `Hello`;
 function playRound(playerSelection, computerSelection) {
-    let message = "";
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        message = "It's a tie";
+        console.log("It's a tie");
     }
     else if (playerSelection.toLowerCase() == "rock") {
         if (computerSelection.toLowerCase() == "scissors") {
-            message = "Rock beats scissors. You win!";
+            console.log("Rock beats scissors. You win!");
+            playerCounter++;
         } else if (computerSelection.toLowerCase() == "paper") {
-            message = "Paper beats rock. You lose.";
+            console.log("Paper beats rock. You lose.");
+            computerCounter++;
         }
     }
     else if (playerSelection.toLowerCase() == "scissors") {
         if (computerSelection.toLowerCase() == "rock") {
-            message = "Rock beats scissors. You lose.";
+            console.log("Rock beats scissors. You lose.");
+            computerCounter++;
         } else if (computerSelection.toLowerCase() == "paper") {
-            message = "Scissors beat paper. You win!";
+            console.log("Scissors beat paper. You win!");
+            playerCounter++;
         }
     }
     else if (playerSelection.toLowerCase() == "paper") {
         if (computerSelection.toLowerCase() == "rock") {
-            message = "Paper beats rock. You win!";
+            console.log("Paper beats rock. You win!");
+            playerCounter++;
         } else if (computerSelection.toLowerCase() == "scissors") {
-            message = "Scissors beat paper. You lose.";
+            console.log("Scissors beat paper. You lose.");
+            computerCounter++;
         }
     }
-    return message;
 }
 
 function game() {
@@ -47,9 +53,19 @@ function game() {
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt("Enter your move: ");
         const computerSelection = getComputerChoice();
-        console.log(computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
+        console.log("Your selection: " + playerSelection);
+        console.log("Computer selection: " + computerSelection);
+        playRound(playerSelection, computerSelection);
     }
+    if(computerCounter > playerCounter){
+        console.log("Computer score: " + computerCounter + " Your score: " + playerCounter);
+        console.log("You lose");
+    } else if (computerCounter < playerCounter){
+        console.log("Computer score:" + computerCounter + " Your score: " + playerCounter);
+        console.log("You win");
+    } else {
+        console.log("Computer score: " + computerCounter + " Your score: " + playerCounter);
+        console.log("It's a tie");
+    }  
 }
 game();
-
