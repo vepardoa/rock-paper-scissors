07435c87456ef0d7c1985ac53481020436186f4d
vepardoa <violetaepardo@gmail.com>
2023-02-25 15:40:34 -0600

    Add play round for rock logic

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let choice = '';
-    let choiceNum = Math.floor(Math.random() * 3 ) + 1;
-    if(choiceNum == 1){
+    let choiceNum = Math.floor(Math.random() * 3) + 1;
+    if (choiceNum == 1) {
         choice = "Rock";
     }
-    else if(choiceNum == 2){
+    else if (choiceNum == 2) {
         choice = "Paper";
     }
     else {
@@ -12,3 +12,17 @@ function getComputerChoice(){
     }
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    let message = "";
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
+        message = "It's a tie";
+        return message;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
